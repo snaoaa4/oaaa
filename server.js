cf63c22a0f97eb46cfbef210348d0cf39993806d
Arezoo Mohandessi <snaoaa4@gmail.com>
2026-02-16 00:08:54 -0800

    Listen on Railway PORT

--- server.js
+++ server.js
@@ -149,5 +149,9 @@ app.post("/logout", (req, res) => {
   req.session.destroy(() => res.redirect("/login"));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+const PORT = Number(process.env.PORT || 8080);
+const HOST = "0.0.0.0";
+
+app.listen(PORT, HOST, () => {
+  console.log(`API running on http://${HOST}:${PORT}`);
+});
